e37364ef29ca4fbeee9499fb17efd5dd07a2b642
Aleksandr Kukhtin <alex.kukhtin.dev@gmail.com>
2020-10-14 11:24:20 +0300

    first commit

---
+++ src/test/java/com/akukhtin/amazonreviewapplication/AmazonReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akukhtin.amazonreviewapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
